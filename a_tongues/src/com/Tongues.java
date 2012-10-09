430609503d3bafd28cf247b06391e8cf4f9a0004
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-09 17:27:32 +0200

    First steps on exercise A

--- a_tongues/src/com/Tongues.java
+++ a_tongues/src/com/Tongues.java
@@ -1,13 +1,32 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author antonio.diaz.arroyo@gmail.com
+ *
+ */
 public class Tongues {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		InputStream inputStream = Tongues.class.getResourceAsStream("A-small-practice.in");
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader bufferReader = new BufferedReader(inputStreamReader);
+		String linea;
+		while ((linea = bufferReader.readLine())!=null) {
+			System.out.println(linea);
+		}
+		bufferReader.close();
+		inputStreamReader.close();
+		inputStream.close();
 	}
 
 }
