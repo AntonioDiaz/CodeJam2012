ecf61c2ce02574aa17c62f56653ad9c220594361
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-09 16:56:18 +0200

    estructure

--- a_tongues/src/Tongues.java
+++
@@ -1,8 +0,0 @@
-
-public class Tonges {
-
-	public static void main(String[] args) {
-		System.out.println("tongues");
-	}
-	
-}
--- b_googlers/src/Googlers.java
+++
@@ -1,8 +0,0 @@
-
-public class Googlers {
-
-	public static void main(String[] args) {
-		System.out.println("googlers");
-	}
-	
-}
