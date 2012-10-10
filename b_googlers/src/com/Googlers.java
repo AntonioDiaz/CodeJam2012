85b94fe619f89ffa3ecd53e2f21f212f47dbfa7a
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-10 17:12:03 +0200

    Structure

---
+++ b_googlers/src/com/Googlers.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ * Problem B. 
+ * @see <a href="http://code.google.com/codejam/contest/1460488/dashboard#s=p1">Dancing With the Googlers</a> 
+ * @author antonio.diaz.arroyo@gmail.com
+ */
+
+public class Googlers {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
