653f3718c0b9e138d9f8d281d82247fad799ceb3
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-10 13:39:27 +0200

    Advancing on the structure.

--- a_tongues/src/com/Tongues.java
+++ a_tongues/src/com/Tongues.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /** 
  * Solution of problem A. Speaking in Tongues
@@ -20,13 +22,31 @@ public class Tongues {
 		InputStream inputStream = Tongues.class.getResourceAsStream("A-small-practice.in");
 		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 		BufferedReader bufferReader = new BufferedReader(inputStreamReader);
-		String linea;
-		while ((linea = bufferReader.readLine())!=null) {
-			System.out.println(linea);
+		int numLines = Integer.parseInt(bufferReader.readLine());
+		String line;
+		List<String> myList = new ArrayList<String>();
+		for (int i = 0; i < numLines; i++) {
+			myList.add(bufferReader.readLine());
+		}
+		List<String> decodeList = Tongues.decodeList(myList);
+		for (int i = 0; i < numLines; i++) {
+			line = decodeList.get(i);
+			line = String.format("Case #%1$d: %2$s ", i+1, line);
+			System.out.println(line);			
+			//System.out.println();			
 		}
 		bufferReader.close();
 		inputStreamReader.close();
 		inputStream.close();
+	}
+	
+	
+	public static List<String> decodeList(List<String> listIn) {
+		List<String> listOut = new ArrayList<String>();
+		for (String string : listIn) {
+			listOut.add(string);
+		}
+		return listOut;
 	}
 
 }
