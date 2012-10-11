40b603d35840f8cae07eb74e65dd7afc4e5f8b4d
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-11 13:40:22 +0200

    Structure

--- b_googlers/src/com/Googlers.java
+++ b_googlers/src/com/Googlers.java
@@ -1,10 +1,14 @@
 package com;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  * Problem B. 
@@ -14,8 +18,11 @@ import java.util.List;
 
 public class Googlers {
 
+	List<Integer> scores;
+	Integer surprising;
+	Integer scoreReference;
+	
 	/**
-	 * 
 	 * @param args
 	 * @throws NumberFormatException
 	 * @throws IOException
@@ -26,50 +33,58 @@ public class Googlers {
 		BufferedReader br = new BufferedReader(inputStreamReader);
 		int numCases = Integer.parseInt(br.readLine()); 
 		for (int i = 0; i < numCases; i++) {
-			System.out.println(br.readLine());
+			System.out.println(new Googlers(br.readLine()));
 		}
-		
 		br.close();
 		inputStreamReader.close();
 		inputStream.close();
 	}
-	
+
 	/**
-	 * 
-	 * @author A153655
-	 *
+	 * @param input a line of the input, format: N S P t1 t2 ... tN
+	 * Where: 
+	 * N is the number of triplets (1 ≤ T ≤ 100)
+	 * S is the number of surprising scores (0 ≤ S ≤ N)
+	 * P is the maximun puntuation (0 ≤ p ≤ 10)
+	 * t is the sum of each triplets
+	 * Example: 3 1 5 15 13 11 
 	 */
-	public class Dancers {
-		public Dancers (String input) {
-			String[] split = input.split(" ");
-			int triplets = Integer.parseInt(split[0]);
-			surprising = new Integer(split[1]);
-			average = new Integer(split[2]);
-		}
-		
-		List<Integer> scores;
-		Integer surprising;
-		Integer average;
-		
-		
-		public List<Integer> getScores() {
-			return scores;
-		}
-		public void setScores(List<Integer> scores) {
-			this.scores = scores;
-		}
-		public Integer getSurprising() {
-			return surprising;
-		}
-		public void setSurprising(Integer surprising) {
-			this.surprising = surprising;
-		}
-		public Integer getAverage() {
-			return average;
-		}
-		public void setAverage(Integer average) {
-			this.average = average;
+	public Googlers (String input) {
+		String[] split = input.split(" ");
+		int triplets = Integer.parseInt(split[0]);
+		surprising = new Integer(split[1]);
+		scoreReference = new Integer(split[2]);
+		scores = new ArrayList<Integer>();
+		for (int i = 2; i < triplets; i++) {
+			scores.add(Integer.parseInt(split[i]));
 		}
 	}
-	
+
+	public Integer getScoreReference() {
+		return scoreReference;
+	}
+	public void setScoreReference(Integer scoreReference) {
+		this.scoreReference = scoreReference;
+	}	
+	public List<Integer> getScores() {
+		return scores;
+	}
+	public void setScores(List<Integer> scores) {
+		this.scores = scores;
+	}
+	public Integer getSurprising() {
+		return surprising;
+	}
+	public void setSurprising(Integer surprising) {
+		this.surprising = surprising;
+	}
+
+	public int calculateTripletsBestResult(){
+		return -1;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
 }
--- b_googlers/src/com/test/GooglersTest.java
+++ b_googlers/src/com/test/GooglersTest.java
@@ -1,13 +1,37 @@
 package com.test;
 
+import com.Googlers;
+
 import junit.framework.TestCase;
 
-
+/**
+ * @author antonio.diaz.arroyo@gmail.com
+ * Test for Qualification Round of Code Jam 2012: Exercise B  
+ */
 public class GooglersTest extends TestCase {
 
+	public void testCalculateTriplets1(){
+		String myInput = "3 1 5 15 13 11";
+		Googlers googlers = new Googlers(myInput);
+		assertEquals (3, googlers.calculateTripletsBestResult());
+	}
 	
-	public void DancerTest (){
+	public void testCalculateTriplets2(){
+		String myInput = "3 0 8 23 22 21";
+		Googlers googlers = new Googlers(myInput);
+		assertEquals (3, googlers.calculateTripletsBestResult());
+	}
 	
+	public void testCalculateTriplets3(){
+		String myInput = "2 1 1 8 0";
+		Googlers googlers = new Googlers(myInput);
+		assertEquals (3, googlers.calculateTripletsBestResult());
+	}
+	
+	public void testCalculateTriplets4(){
+		String myInput = "6 2 8 29 20 8 18 18 21";
+		Googlers googlers = new Googlers(myInput);
+		assertEquals (3, googlers.calculateTripletsBestResult());
 	}
 	
 }
