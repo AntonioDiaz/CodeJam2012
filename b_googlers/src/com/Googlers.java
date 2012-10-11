89bc2f1907c6c5a1eecf39309cc6db17844ff922
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-11 11:04:47 +0200

    structure

--- b_googlers/src/com/Googlers.java
+++ b_googlers/src/com/Googlers.java
@@ -1,5 +1,11 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
 /**
  * Problem B. 
  * @see <a href="http://code.google.com/codejam/contest/1460488/dashboard#s=p1">Dancing With the Googlers</a> 
@@ -8,8 +14,62 @@ package com;
 
 public class Googlers {
 
-	public static void main(String[] args) {
+	/**
+	 * 
+	 * @param args
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		InputStream inputStream = Googlers.class.getResourceAsStream("B-small-practice.in");
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader br = new BufferedReader(inputStreamReader);
+		int numCases = Integer.parseInt(br.readLine()); 
+		for (int i = 0; i < numCases; i++) {
+			System.out.println(br.readLine());
+		}
 		
+		br.close();
+		inputStreamReader.close();
+		inputStream.close();
+	}
+	
+	/**
+	 * 
+	 * @author A153655
+	 *
+	 */
+	public class Dancers {
+		public Dancers (String input) {
+			String[] split = input.split(" ");
+			int triplets = Integer.parseInt(split[0]);
+			surprising = new Integer(split[1]);
+			average = new Integer(split[2]);
+		}
+		
+		List<Integer> scores;
+		Integer surprising;
+		Integer average;
+		
+		
+		public List<Integer> getScores() {
+			return scores;
+		}
+		public void setScores(List<Integer> scores) {
+			this.scores = scores;
+		}
+		public Integer getSurprising() {
+			return surprising;
+		}
+		public void setSurprising(Integer surprising) {
+			this.surprising = surprising;
+		}
+		public Integer getAverage() {
+			return average;
+		}
+		public void setAverage(Integer average) {
+			this.average = average;
+		}
 	}
 	
 }
