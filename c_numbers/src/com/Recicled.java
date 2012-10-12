10411647ef9607420c38610e739e395dc1352b08
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-12 16:07:22 +0200

    structure

--- c_numbers/src/com/Recicled.java
+++ c_numbers/src/com/Recicled.java
@@ -1,5 +1,10 @@
 package com;
 
+/**
+ * Problem C. 
+ * @see <a href="http://code.google.com/codejam/contest/1460488/dashboard#s=p2">Problem C. Recycled Numbers</a> 
+ * @author antonio.diaz.arroyo@gmail.com
+ */
 public class Recicled {
 
 	public static void main(String[] args) {
--- c_numbers/src/com/test/TestRecicled.java
+++ c_numbers/src/com/test/TestRecicled.java
@@ -2,6 +2,10 @@ package com.test;
 
 import junit.framework.TestCase;
 
+/**
+ * @author antonio.diaz.arroyo@gmail.com
+ * Test for Qualification Round of Code Jam 2012: Exercise C  
+ */
 public class TestRecicled extends TestCase {
 
 }
