25c85453a75863201181e86b3e66f710682686b6
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-15 09:39:49 +0200

    structure

--- c_numbers/src/com/Recicled.java
+++ c_numbers/src/com/Recicled.java
@@ -1,5 +1,9 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Problem C. 
  * @see <a href="http://code.google.com/codejam/contest/1460488/dashboard#s=p2">Problem C. Recycled Numbers</a> 
@@ -7,8 +11,27 @@ package com;
  */
 public class Recicled {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+		InputStream inputStream = Recicled.class.getResourceAsStream("C-small-practice.in");
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader bf = new BufferedReader(inputStreamReader);
+		int cases = Integer.parseInt(bf.readLine());
+		for (int i = 0; i < cases; i++) {
+			int recicledPairs = Recicled.calculateRecicledPairs(bf.readLine());
+			System.out.println(String.format("Case #%1$d: %2$s ", i+1, recicledPairs));
+		}
 	}
-	
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static int calculateRecicledPairs(String myLine) {
+		String[] split = myLine.split(" ");
+		int a = Integer.parseInt(split[0]);
+		int b = Integer.parseInt(split[1]);
+		return -1;
+	}
 }
--- c_numbers/src/com/test/TestRecicled.java
+++ c_numbers/src/com/test/TestRecicled.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import com.Recicled;
+
 import junit.framework.TestCase;
 
 /**
@@ -8,4 +10,27 @@ import junit.framework.TestCase;
  */
 public class TestRecicled extends TestCase {
 
+	public void testCalculateRecicledPairs01(){
+		String inStr = "1 9";
+		int result = 0;
+		assertEquals(result, Recicled.calculateRecicledPairs(inStr));
+	}
+	
+	public void testCalculateRecicledPairs02(){
+		String inStr = "10 40";
+		int result = 3;
+		assertEquals(result, Recicled.calculateRecicledPairs(inStr));
+	}
+	
+	public void testCalculateRecicledPairs03(){
+		String inStr = "100 500";
+		int result = 0;
+		assertEquals(result, Recicled.calculateRecicledPairs(inStr));
+	}
+	
+	public void testCalculateRecicledPairs04(){
+		String inStr = "1111 2222";
+		int result = 0;
+		assertEquals(result, Recicled.calculateRecicledPairs(inStr));
+	}
 }
