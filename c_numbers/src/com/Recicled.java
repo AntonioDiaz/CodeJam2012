2940f36d8d409bb50161bdc090a97353a28d8e5a
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-15 11:52:49 +0200

    algorithm almost finished...

--- c_numbers/src/com/Recicled.java
+++ c_numbers/src/com/Recicled.java
@@ -12,7 +12,7 @@ import java.io.InputStreamReader;
 public class Recicled {
 
 	public static void main(String[] args) throws Exception {
-		InputStream inputStream = Recicled.class.getResourceAsStream("C-small-practice.in");
+		InputStream inputStream = Recicled.class.getResourceAsStream("C-large-practice.in");
 		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 		BufferedReader bf = new BufferedReader(inputStreamReader);
 		int cases = Integer.parseInt(bf.readLine());
@@ -21,7 +21,7 @@ public class Recicled {
 			System.out.println(String.format("Case #%1$d: %2$s ", i+1, recicledPairs));
 		}
 	}
-
+	
 	/**
 	 * 
 	 * @param a
@@ -32,6 +32,21 @@ public class Recicled {
 		String[] split = myLine.split(" ");
 		int a = Integer.parseInt(split[0]);
 		int b = Integer.parseInt(split[1]);
-		return -1;
+		int bDigits = String.valueOf(b).length();
+		int pairsFounds = 0;
+		for (int i = a; i < b; i++) {
+			for (int j=1; j<bDigits; j++){
+				/* if i=1 --> 119 -> 911 */ 
+				/* if i=2 --> 119 -> 191 */ 
+				int newA = i%(int)(Math.pow(10,j));
+				newA *= Math.pow(10,(bDigits-j));
+				newA += i/Math.pow(10,j);
+				int newADigits = String.valueOf(newA).length();
+				if (i<newA && newA<=b && bDigits==newADigits){
+					pairsFounds++;
+				}
+			}
+		}
+		return pairsFounds;
 	}
 }
