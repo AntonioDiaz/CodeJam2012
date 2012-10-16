de301f295576d1a4b29bd5f2c2e67c7ad56f2b2a
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-10-16 15:41:11 +0200

    advances on the structure

--- d_mirrors/src/com/Mirror.java
+++ d_mirrors/src/com/Mirror.java
@@ -1,5 +1,11 @@
 package com;
 
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 /**
  * Google Code Jam 2012 - Qualification Round exercise D.
  * @see <a href="http://code.google.com/codejam/contest/1460488/dashboard#s=p3">Hall of Mirrors</a> 
@@ -10,13 +16,88 @@ public class Mirror {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
-
-
+	public static void main(String[] args) throws Exception {
+		InputStream inputStream = Mirror.class.getResourceAsStream("D-small-practice.in");
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader bf = new BufferedReader(inputStreamReader);
+		int cases = Integer.parseInt(bf.readLine());
+		for (int i = 0; i < cases; i++) {
+			String firstLine = bf.readLine();
+			int h = Integer.parseInt(firstLine.split(" ")[0]);
+			StringBuilder stb = new StringBuilder();
+			for (int j = 0; j < h; j++) {
+				stb.append(bf.readLine());
+			}
+			Mirror myMirror = new Mirror(firstLine, stb.toString());
+			int reflections = myMirror.calculateReflections();
+			System.out.println(myMirror.toString());
+			System.out.println(String.format("Case #%1$d: %2$s ", i+1, reflections));
+		}
+		bf.close();
+		inputStreamReader.close();
+		inputStream.close();
 	}
 	
-	public static final int calculateReflections(String strIn){
+	private int height;
+	private int width;
+	private int distance;
+	private byte[][] grid; 
+	
+	public Mirror (String gridDates, String gridStr) {
+		height = Integer.parseInt(gridDates.split(" ")[0]);
+		width = Integer.parseInt(gridDates.split(" ")[1]);
+		distance = Integer.parseInt(gridDates.split(" ")[2]);
+		grid = new byte[height][width];
+		for (int row = 0; row < height; row++) {
+			for (int column = 0; column < width; column++) {
+				grid[row][column] = (byte)gridStr.charAt(column+(row*width));
+			}
+		}
+	}
+	
+	public int calculateReflections(){
+		System.out.println(this.toString());
 		return -1;
 	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	public byte[][] getGrid() {
+		return grid;
+	}
+
+
+	public void setGrid(byte[][] grid) {
+		this.grid = grid;
+	}
+	
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
 }
--- d_mirrors/src/com/test/TestMirror.java
+++ d_mirrors/src/com/test/TestMirror.java
@@ -10,79 +10,16 @@ import junit.framework.TestCase;
  */
 public class TestMirror extends TestCase{
 
+	/**
+	 * Test 01.
+	 */
 	public void testMirror01(){
-		int expected = 4;
-		String strIn = 
-				"3 3 1" +
-				"###" +
-				"#X#" +
-				"###";
-		System.out.println(strIn);
-		assertEquals(expected, Mirror.calculateReflections(strIn));
-	}
-	
-	public void testMirror02(){
-		int expected = 8;
-		String strIn = 
-				"3 3 2" +
-				"###" +
-				"#X#" +
-				"###";
-		System.out.println(strIn);
-		assertEquals(expected, Mirror.calculateReflections(strIn));
-	}
-	
-	public void testMirror03(){
-		int expected = 68;
-		String strIn = 
-				"4 3 8" +
-				"###" +
-				"#X#" +
-				"#.#" +
-				"###";
-		System.out.println(strIn);
-		assertEquals(expected, Mirror.calculateReflections(strIn));
-	}
-	
-	public void testMirror04(){
 		int expected = 0;
-		String strIn = 
-				"7 7 4" +
-				"#######" +
-				"#.....#" +
-				"#.....#" +
-				"#..X..#" +
-				"#....##" +
-				"#.....#" +
-				"#######";
-		System.out.println(strIn);
-		assertEquals(expected, Mirror.calculateReflections(strIn));
+		String strInDates = "3 3 1";
+		String strInGrid = 
+				"###" +
+				"#X#" +
+				"###";
+		assertEquals(expected, new Mirror(strInDates, strInGrid).calculateReflections());
 	}
-	
-	public void testMirror05(){
-		int expected = 2;
-		String strIn = 
-				"5 6 3" +
-				"######" +
-				"#..X.#" +
-				"#.#..#" +
-				"#...##" +
-				"######";
-				System.out.println(strIn);
-		assertEquals(expected, Mirror.calculateReflections(strIn));
-	}
-	
-	public void testMirror06(){
-		int expected = 28;
-		String strIn = 
-				"5 6 10" +
-				"######" +
-				"#..X.#" +
-				"#.#..#" +
-				"#...##" +
-				"######";
-		System.out.println(strIn);
-		assertEquals(expected, Mirror.calculateReflections(strIn));
-	}
-	
 }
